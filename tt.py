0fc6acf0f13663b93912d546e8cd8efeaa01e65e
aakwvh <agus.vanhuut@student.hu.nl>
2023-06-27 02:23:45 +0200

    in deze commit heb ik de functie cosine similarity gecodeerd hiermee kan je berekenen hoe vector x en y te vergelijkbaar zijn. daarnaast heb ik ook functie die aanbevolen film gecodeerd

--- tt.py
+++ tt.py
@@ -38,5 +38,58 @@ def preprocess_genres(df_movies):
     return df_genres
 
 
+def cosine_similarity(x, y):
+    """ bron: https://www.learndatasci.com/glossary/cosine-similarity/
 
+    formule: similarity(x,y) = cos(𝑋⃗, 𝑌⃗) = 𝑋⃗⃗.𝑌⃗⃗ / |𝑋|.|𝑌|
+    𝑋⃗⃗.𝑌⃗⃗ is Bereken het dot-product ofterwel inwendig product tussen x en y.
+    Dus stel je voor D1 = [1,1,0,0] en D2 = [0,1,1,0], D1 * D2 = 1*0 + 1*1 + 0*1 + 0*0 = 1
+    |𝑋|.|𝑌| hier berekenen we de magnitude van X en Y.
+
+    met de cosine similarty kan je berekenen hoe vector x en y te vergelijkbaar zijn.
+    Dus ze kijken hoe vergelijkbaar film a en film b. als je kijk naar bepaalde kenmerken van dat film in dit geval kijken we naar de genres.
+
+    :return: een float tussen de 1 en de 0 dus hoe dichter bij de 1 hoe vergelijkbaar de de twee films en als dichtbij de 0 dan verschillend ze zijn van elkaar.
+    """
+
+    # Zorg ervoor dat de lengte van x en y hetzelfde is
+    if len(x) != len(y):
+        return None
+
+    # Bereken het dot-product ofterwel inwendig product tussen x en y
+    # dus stel je voor D1 = [1,1,0,0] en D2 = [0,1,1,0]
+    # D1 * D2 = 1*0 + 1*1 + 0*1 + 0*0 = 1
+    dot_product = inwendig_product(x, y)
+
+    # Bereken de L2-normen (magnitude) van x en y
+    magnitude_x = calculate_magnitude(x)
+    magnitude_y = calculate_magnitude(y)
+
+    # Bereken de cosine similarity
+    cosine_similarity = dot_product / (magnitude_x * magnitude_y)
+
+    return cosine_similarity
+
+
+
+def recommend_similar_movies(df_genres, liked_movie_index, num_recommendations=5):
+    """
+    Genereer aanbevolen films op basis van de gelijkenis met een gegeven film.
+    :param df_genres: Gegevensframe met genres
+    :param liked_movie_index: Index van de gewaardeerde film
+    :param num_recommendations: Aantal aanbevolen films om te genereren
+    :return: Gegevensframe met aanbevolen films
+    """
+    movies_array = df_genres.values
+    liked_movie = df_movies.loc[liked_movie_index, 'movie_title']
+    liked_movie_vector = movies_array[liked_movie_index]
+    similarities = []
+    for i, movie_vector in enumerate(movies_array):
+        if i != liked_movie_index:
+            similarity = cosine_similarity(liked_movie_vector, movie_vector)
+            similarities.append((i, similarity))
+    similarities.sort(key=lambda x: x[1], reverse=True)
+    top_recommendations = [index for index, _ in similarities[:num_recommendations]]
+    recommended_movies = df_movies.loc[top_recommendations, 'movie_title']
+    return recommended_movies
 
